e8dd09d170b2b1b363ac77a6f64fe13115f88138
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-05-10 16:14:36 -0400

    Add attachCoinListeners() to coins.js

--- app/assets/javascripts/coins.js
+++ app/assets/javascripts/coins.js
@@ -1,20 +1,36 @@
  // Make JS objects out of CMC API Data
  // https://coinmarketcap.com/api/
 
- function getCoinData() {
-   $.get('https://api.coinmarketcap.com/v2/listings/', function (resp) {
-     let coins = resp["data"]
-     for (let coinData of coins) {
-       for (let data in object) {
-         coin = new Coin()
-       }
-     }
-   })
- }
+$(document).ready(function () {
+  attachCoinListeners();
+});
 
- function Coin(name, symbol, id) {
-   
- }
+function attachCoinListeners() {
+
+}
+
+function getCoinData() {
+  $.get('https://api.coinmarketcap.com/v2/listings/', function (resp) {
+    let coins = resp["data"]
+    for (let coinData of coins) {
+      coin = new Coin(coinData["name"], coinData["symbol"], coinData["id"])
+      $.post('/coins', {}, function (coin) {
+        // post to data-list /api?
+      })
+    }
+  });
+}
+
+// let coinId = 0;
+// Should I use CMC's ID or rely on creating my own?
+
+function Coin(name, symbol, id) {
+  this.name = name;
+  this.symbol = symbol;
+  this.id = id;
+
+  // coinId++;
+}
 
 /*
 Sample Response:
@@ -41,3 +57,5 @@ Sample Response:
     }
 ]
 */
+
+// Alternatively, I can create and hit my own API endpoint displaying coin data, but I would not be actively managing this data. Perhaps, it's better to rely on a site like CoinMarketCap.
