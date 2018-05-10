4df217fc2814d6b28fe3ec3fdf89c02174ab4b45
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-05-10 15:59:43 -0400

    Add Coin constructor function to coins.js

--- app/assets/javascripts/coins.js
+++ app/assets/javascripts/coins.js
@@ -1,10 +1,19 @@
  // Make JS objects out of CMC API Data
  // https://coinmarketcap.com/api/
- 
- function getCoinData() {
-   $.get('https://api.coinmarketcap.com/v2/listings/', function () {
 
+ function getCoinData() {
+   $.get('https://api.coinmarketcap.com/v2/listings/', function (resp) {
+     let coins = resp["data"]
+     for (let coinData of coins) {
+       for (let data in object) {
+         coin = new Coin()
+       }
+     }
    })
+ }
+
+ function Coin(name, symbol, id) {
+   
  }
 
 /*
