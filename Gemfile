df4c6aaa0a3bee8510fd29e8c2a055b73b590aa5
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-05-11 13:42:16 -0400

    Get javascript loaded properly in layout head

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 gem 'nokogiri'
+gem 'jquery-rails'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,16 +1,30 @@
-let commentId = 0;
+// let commentId = 0;
+//
+// function Comment(content) {
+//   this.content = content
+//   this.commentId = commentId++;
+//
+//   commentId++;
+// }
 
-function Comment(content) {
-  this.content = content
-  this.commentId = commentId++;
+// function createComment() {
+//   // let review = $('#review-')
+//   $('#comment-btn').on('click', function () {
+//     // $('#comment-section').append("Hello")
+//     alert("you clicked add comment!!!")
+//
+//     // (`<input type="text" placeholder="Your comment here..."></input>`)
+//   })
+//   // $.fetch(`/reviews/${}`)
+// }
 
-  commentId++;
-}
+$(document).ready(function () {
+  attachCommentListeners();
+})
 
-function createComment() {
-  // let review = $('#review-')
-  $('#comment-btn').on('click', function () {
-    $('#comment-section').append(`<input type="text" placeholder="Your comment here..."></input>`)
+function attachCommentListeners() {
+  $('#comment-link').on('click', function (e) {
+    e.preventDefault();
+    alert("you clicked me!");
   })
-  // $.fetch(`/reviews/${}`)
 }
