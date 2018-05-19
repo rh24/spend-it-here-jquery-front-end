3cf1cea296d3b0d6224ed672ac472851e29d3f0d
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-05-19 11:45:12 -0400

    test commit

--- config/routes.rb
+++ config/routes.rb
@@ -26,3 +26,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # get '/businesses/:bitcoin' => 'spendables#bitcoin' as:
 end
+
+test
