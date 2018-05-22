d7c583b7c9f5754207b15143e8c6a79c390a0a74
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-05-22 19:52:46 -0400

    Clean up assets/stylesheets and load scripts for bootstrap in application.html.erb head

---
+++ app/assets/javascripts/reviews.js
@@ -0,0 +1,13 @@
+$('#myReviews').on('click', function (e) {
+  e.preventDefault();
+  $.get(`/posts.json`, function (data) {
+    data.forEach(function (el) {
+      $('#posts-index').append(`
+        <div id="post-${el["id"]}">
+        <h4><a href="/posts/${el["id"]}">${el["title"]}</a></h4>
+        <p>${el["content"]}</p>
+        </div>
+      `)
+    })
+  })
+})
